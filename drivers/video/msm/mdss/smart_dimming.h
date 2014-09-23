2ac2af946e2f0f9616e7fedb285e1d8c10e63404
solk2 <solk@solk.org.ua>
2014-09-23 14:34:57 +0300

    msm: mdss: Moved mipi_samsung_cmd_list to smart_dimming.h.
    Change-Id: If4a7a12de9191d9eb928a100c4de642c892b18f7

--- drivers/video/msm/mdss/smart_dimming.h
+++ drivers/video/msm/mdss/smart_dimming.h
@@ -35,5 +35,32 @@ struct smartdim_conf_hmt {
 struct smartdim_conf_hmt *smart_S6E3_get_conf_hmt(int dual);
 #endif
 
+enum mipi_samsung_cmd_list {
+
+        PANEL_READY_TO_ON,
+        PANEL_DISP_OFF,
+        PANEL_DISPLAY_ON,
+        PANEL_DISPLAY_OFF,
+        PANEL_DISPLAY_UNBLANK,
+        PANEL_DISPLAY_BLANK,
+        PANEL_ALL_PIXEL_OFF,
+        PANEL_BRIGHT_CTRL,
+        PANEL_MTP_ENABLE,
+        PANEL_MTP_DISABLE,
+        PANEL_NEED_FLIP,
+        PANEL_ACL_OFF,
+        PANEL_ACL_ON,
+        PANEL_LATE_ON,
+        PANEL_EARLY_OFF,
+        PANEL_TOUCHSENSING_ON,
+        PANEL_TOUCHSENSING_OFF,
+        PANEL_TEAR_ON,
+        PANEL_TEAR_OFF,
+        PANEL_LDI_FPS_CHANGE,
+        PANEL_LDI_SET_VDDM_OFFSET, /*LDI_ADJ_VDDM_OFFSET*/
+        PANEL_PARTIAL_ON,
+        PANEL_PARTIAL_OFF,
+        PANEl_FORCE_500CD
+};
 
 #endif /* _SMART_DIMMING_H_ */
--- drivers/video/msm/mdss/smart_mtp_se6e8fa.c
+++ drivers/video/msm/mdss/smart_mtp_se6e8fa.c
@@ -115,34 +115,6 @@ extern int v23_val[3];
 extern int v11_val[3];
 extern int v3_val[3];
 
-enum mipi_samsung_cmd_list {
-
-        PANEL_READY_TO_ON,
-        PANEL_DISP_OFF,
-        PANEL_DISPLAY_ON,
-        PANEL_DISPLAY_OFF,
-        PANEL_DISPLAY_UNBLANK,
-        PANEL_DISPLAY_BLANK,
-        PANEL_ALL_PIXEL_OFF,
-        PANEL_BRIGHT_CTRL,
-        PANEL_MTP_ENABLE,
-        PANEL_MTP_DISABLE,
-        PANEL_NEED_FLIP,
-        PANEL_ACL_OFF,
-        PANEL_ACL_ON,
-        PANEL_LATE_ON,
-        PANEL_EARLY_OFF,
-        PANEL_TOUCHSENSING_ON,
-        PANEL_TOUCHSENSING_OFF,
-        PANEL_TEAR_ON,
-        PANEL_TEAR_OFF,
-        PANEL_LDI_FPS_CHANGE,
-        PANEL_LDI_SET_VDDM_OFFSET, /*LDI_ADJ_VDDM_OFFSET*/
-        PANEL_PARTIAL_ON,
-        PANEL_PARTIAL_OFF,
-        PANEl_FORCE_500CD
-};
-
 extern int mipi_samsung_disp_send_cmd(
 	enum mipi_samsung_cmd_list cmd,
 	unsigned char lock);
