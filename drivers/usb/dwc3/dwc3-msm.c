1743b9bee5a25a17af0948b14cc5efecf92a96b8
solk2 <solk@solk.org.ua>
2014-11-28 00:32:01 +0200

    dwc3: Revert dwc3 changes for now.
    Change-Id: I221001cca269522de05747636338a202227a9a22

--- drivers/usb/dwc3/dwc3-msm.c
+++ drivers/usb/dwc3/dwc3-msm.c
@@ -189,8 +189,6 @@ struct dwc3_msm {
 	struct clk		*sleep_clk;
 	struct clk		*hsphy_sleep_clk;
 	struct clk		*utmi_clk;
-	unsigned int		utmi_clk_rate;
-	struct clk		*utmi_clk_src;
 	struct regulator	*hsusb_3p3;
 	struct regulator	*hsusb_1p8;
 	struct regulator	*hsusb_vddcx;
@@ -241,7 +239,6 @@ struct dwc3_msm {
 	unsigned long		lpm_flags;
 #define MDWC3_PHY_REF_AND_CORECLK_OFF	BIT(0)
 #define MDWC3_TCXO_SHUTDOWN		BIT(1)
-#define MDWC3_ASYNC_IRQ_WAKE_CAPABILITY	BIT(2)
 
 	u32 qscratch_ctl_val;
 	dev_t ext_chg_dev;
@@ -900,7 +897,7 @@ static int dwc3_msm_ep_queue(struct usb_ep *ep,
 	 * Override req->complete function, but before doing that,
 	 * store it's original pointer in the req_complete_list.
 	 */
-	req_complete = kzalloc(sizeof(*req_complete), gfp_flags);
+	req_complete = kzalloc(sizeof(*req_complete), GFP_KERNEL);
 	if (!req_complete) {
 		dev_err(mdwc->dev, "%s: not enough memory\n", __func__);
 		return -ENOMEM;
@@ -1102,7 +1099,7 @@ void msm_dwc3_restart_usb_session(struct usb_gadget *gadget)
 	struct dwc3 *dwc = container_of(gadget, struct dwc3, gadget);
 	struct dwc3_msm *mdwc = dev_get_drvdata(dwc->dev->parent);
 
-	if (!mdwc)
+	if (mdwc)
 		return;
 
 	dev_dbg(mdwc->dev, "%s\n", __func__);
@@ -1467,48 +1464,6 @@ static void dwc3_msm_ss_phy_reg_init(struct dwc3_msm *mdwc)
 				0x07f03f07, 0x07f01605);
 }
 
-static void dwc3_msm_update_ref_clk(struct dwc3_msm *mdwc)
-{
-	u32 guctl, gfladj = 0;
-
-	guctl = dwc3_msm_read_reg(mdwc->base, DWC3_GUCTL);
-	guctl &= ~DWC3_GUCTL_REFCLKPER;
-
-	/* GFLADJ register is used starting with revision 2.50a */
-	if (dwc3_msm_read_reg(mdwc->base, DWC3_GSNPSID) >= DWC3_REVISION_250A) {
-		gfladj = dwc3_msm_read_reg(mdwc->base, DWC3_GFLADJ);
-		gfladj &= ~DWC3_GFLADJ_REFCLK_240MHZDECR_PLS1;
-		gfladj &= ~DWC3_GFLADJ_REFCLK_240MHZ_DECR;
-		gfladj &= ~DWC3_GFLADJ_REFCLK_LPM_SEL;
-		gfladj &= ~DWC3_GFLADJ_REFCLK_FLADJ;
-	}
-
-	/* Refer to SNPS Databook Table 6-55 for calculations used */
-	switch (mdwc->utmi_clk_rate) {
-	case 19200000:
-		guctl |= 52 << __ffs(DWC3_GUCTL_REFCLKPER);
-		gfladj |= 12 << __ffs(DWC3_GFLADJ_REFCLK_240MHZ_DECR);
-		gfladj |= DWC3_GFLADJ_REFCLK_240MHZDECR_PLS1;
-		gfladj |= DWC3_GFLADJ_REFCLK_LPM_SEL;
-		gfladj |= 200 << __ffs(DWC3_GFLADJ_REFCLK_FLADJ);
-		break;
-	case 24000000:
-		guctl |= 41 << __ffs(DWC3_GUCTL_REFCLKPER);
-		gfladj |= 10 << __ffs(DWC3_GFLADJ_REFCLK_240MHZ_DECR);
-		gfladj |= DWC3_GFLADJ_REFCLK_LPM_SEL;
-		gfladj |= 2032 << __ffs(DWC3_GFLADJ_REFCLK_FLADJ);
-		break;
-	default:
-		dev_warn(mdwc->dev, "Unsupported utmi_clk_rate: %u\n",
-				mdwc->utmi_clk_rate);
-		break;
-	}
-
-	dwc3_msm_write_reg(mdwc->base, DWC3_GUCTL, guctl);
-	if (gfladj)
-		dwc3_msm_write_reg(mdwc->base, DWC3_GFLADJ, gfladj);
-}
-
 /* Initialize QSCRATCH registers for HSPHY and SSPHY operation */
 static void dwc3_msm_qscratch_reg_init(struct dwc3_msm *mdwc,
 						unsigned event_status)
@@ -1592,7 +1547,6 @@ static void dwc3_msm_notify_event(struct dwc3 *dwc, unsigned event)
 				"DWC3_CONTROLLER_POST_RESET_EVENT received\n");
 		dwc3_msm_qscratch_reg_init(mdwc,
 					DWC3_CONTROLLER_POST_RESET_EVENT);
-		dwc3_msm_update_ref_clk(mdwc);
 		dwc->tx_fifo_size = mdwc->tx_fifo_size;
 		break;
 	case DWC3_CONTROLLER_POST_INITIALIZATION_EVENT:
@@ -1633,29 +1587,10 @@ static void dwc3_block_reset_usb_work(struct work_struct *w)
 {
 	struct dwc3_msm *mdwc = container_of(w, struct dwc3_msm,
 						usb_block_reset_work);
-	u32 reg;
 
 	dev_dbg(mdwc->dev, "%s\n", __func__);
 
 	dwc3_msm_block_reset(&mdwc->ext_xceiv, true);
-
-	reg = (DWC3_DEVTEN_EVNTOVERFLOWEN |
-			DWC3_DEVTEN_CMDCMPLTEN |
-			DWC3_DEVTEN_ERRTICERREN |
-			DWC3_DEVTEN_WKUPEVTEN |
-			DWC3_DEVTEN_CONNECTDONEEN |
-			DWC3_DEVTEN_USBRSTEN |
-			DWC3_DEVTEN_DISCONNEVTEN);
-	/*
-	 * Enable SUSPENDEVENT(BIT:6) for version 230A and above
-	 * else enable USB Link change event (BIT:3) for older version
-	 */
-	if (dwc3_msm_read_reg(mdwc->base, DWC3_GSNPSID) < DWC3_REVISION_230A)
-		reg |= DWC3_DEVTEN_ULSTCNGEN;
-	else
-		reg |= DWC3_DEVTEN_SUSPEND;
- 
-	dwc3_msm_write_reg(mdwc->base, DWC3_DEVTEN, reg);
 }
 
 static void dwc3_chg_enable_secondary_det(struct dwc3_msm *mdwc)
@@ -1888,7 +1823,6 @@ static int dwc3_msm_suspend(struct dwc3_msm *mdwc)
 	bool host_bus_suspend;
 	bool host_ss_active;
 	bool host_ss_suspend;
-	bool device_bus_suspend;
 
 #ifdef CONFIG_USB_DEBUG_DETEAILED_LOG
 	dev_info(mdwc->dev, "%s: entering lpm\n", __func__);
@@ -1920,8 +1854,6 @@ static int dwc3_msm_suspend(struct dwc3_msm *mdwc)
 	      (mdwc->charger.chg_type == DWC3_FLOATED_CHARGER));
 	host_bus_suspend = mdwc->host_mode == 1;
 	host_ss_suspend = host_bus_suspend && host_ss_active;
-	device_bus_suspend = ((mdwc->charger.chg_type == DWC3_SDP_CHARGER) ||
-				 (mdwc->charger.chg_type == DWC3_CDP_CHARGER));
 
 	if (!dcp && !host_bus_suspend)
 		dwc3_msm_write_reg(mdwc->base, QSCRATCH_CTRL_REG,
@@ -1994,12 +1926,16 @@ static int dwc3_msm_suspend(struct dwc3_msm *mdwc)
 	if (!host_bus_suspend)
 		dwc3_msm_config_gdsc(mdwc, 0);
 
-	clk_disable_unprepare(mdwc->iface_clk);
-	clk_disable_unprepare(mdwc->utmi_clk);
-
 	if (!host_ss_suspend) {
 		clk_disable_unprepare(mdwc->core_clk);
 		mdwc->lpm_flags |= MDWC3_PHY_REF_AND_CORECLK_OFF;
+	}
+	clk_disable_unprepare(mdwc->iface_clk);
+
+	if (!host_bus_suspend)
+		clk_disable_unprepare(mdwc->utmi_clk);
+
+	if (!host_bus_suspend) {
 		/* USB PHY no more requires TCXO */
 		clk_disable_unprepare(mdwc->xo_clk);
 		mdwc->lpm_flags |= MDWC3_TCXO_SHUTDOWN;
@@ -2026,16 +1962,10 @@ static int dwc3_msm_suspend(struct dwc3_msm *mdwc)
 	dev_info(mdwc->dev, "DWC3 in low power mode\n");
 
 	if (mdwc->hs_phy_irq) {
-		/*
-		 * with DCP or during cable disconnect, we dont require wakeup
-		 * using HS_PHY_IRQ. Hence enable wakeup only in case of host
-		 * bus suspend and device bus suspend.
-		 */
-		if (host_bus_suspend || device_bus_suspend) {
-			enable_irq_wake(mdwc->hs_phy_irq);
-			mdwc->lpm_flags |= MDWC3_ASYNC_IRQ_WAKE_CAPABILITY;
-		}
 		enable_irq(mdwc->hs_phy_irq);
+		/* with DCP we dont require wakeup using HS_PHY_IRQ */
+		if (dcp || !mdwc->vbus_active) // add SAMSUNG
+			disable_irq_wake(mdwc->hs_phy_irq);
 	}
 
 	return 0;
@@ -2089,7 +2019,8 @@ static int dwc3_msm_resume(struct dwc3_msm *mdwc)
 	if (!host_bus_suspend)
 		dwc3_msm_config_gdsc(mdwc, 1);
 
-	clk_prepare_enable(mdwc->utmi_clk);
+	if (!host_bus_suspend)
+		clk_prepare_enable(mdwc->utmi_clk);
 
 	if (mdwc->otg_xceiv && mdwc->ext_xceiv.otg_capability && !dcp &&
 							!host_bus_suspend)
@@ -2178,12 +2109,9 @@ static int dwc3_msm_resume(struct dwc3_msm *mdwc)
 		enable_irq(mdwc->hs_phy_irq);
 		mdwc->lpm_irq_seen = false;
 	}
-	/* Disable wakeup capable for HS_PHY IRQ, if enabled */
-	if (mdwc->hs_phy_irq &&
-			(mdwc->lpm_flags & MDWC3_ASYNC_IRQ_WAKE_CAPABILITY)) {
-			disable_irq_wake(mdwc->hs_phy_irq);
-			mdwc->lpm_flags &= ~MDWC3_ASYNC_IRQ_WAKE_CAPABILITY;
-	}
+	/* it must DCP disconnect, re-enable HS_PHY wakeup IRQ */
+	if ((mdwc->hs_phy_irq && dcp) || !mdwc->vbus_active) // add SAMSUNG
+		enable_irq_wake(mdwc->hs_phy_irq);
 
 	dev_info(mdwc->dev, "DWC3 exited from low power mode\n");
 
@@ -2924,35 +2852,11 @@ static int __devinit dwc3_msm_probe(struct platform_device *pdev)
 	}
 	clk_prepare_enable(mdwc->hsphy_sleep_clk);
 
-	ret = of_property_read_u32(node, "qcom,utmi-clk-rate",
-				   (u32 *)&mdwc->utmi_clk_rate);
-	if (ret)
-		mdwc->utmi_clk_rate = 60000000;
-
 	mdwc->utmi_clk = devm_clk_get(&pdev->dev, "utmi_clk");
 	if (IS_ERR(mdwc->utmi_clk)) {
 		dev_err(&pdev->dev, "failed to get utmi_clk\n");
 		ret = PTR_ERR(mdwc->utmi_clk);
 		goto disable_sleep_a_clk;
-	}
-
-	if (mdwc->utmi_clk_rate == 24000000) {
-		/*
-		 * For setting utmi clock to 24MHz, first set 48MHz on parent
-		 * clock "utmi_clk_src" and then set divider 2 on child branch
-		 * "utmi_clk".
-		 */
-		mdwc->utmi_clk_src = devm_clk_get(&pdev->dev, "utmi_clk_src");
-		if (IS_ERR(mdwc->utmi_clk_src)) {
-			dev_err(&pdev->dev, "failed to get utmi_clk_src\n");
-			ret = PTR_ERR(mdwc->utmi_clk_src);
-			goto disable_sleep_a_clk;
-		}
-		clk_set_rate(mdwc->utmi_clk_src, 48000000);
-		/* 1 means divide utmi_clk_src by 2 */
-		clk_set_rate(mdwc->utmi_clk, 1);
-	} else {
-		clk_set_rate(mdwc->utmi_clk, mdwc->utmi_clk_rate);
 	}
 	clk_prepare_enable(mdwc->utmi_clk);
 
@@ -3086,6 +2990,7 @@ static int __devinit dwc3_msm_probe(struct platform_device *pdev)
 			dev_err(&pdev->dev, "irqreq HSPHYINT failed\n");
 			goto disable_hs_ldo;
 		}
+		enable_irq_wake(mdwc->hs_phy_irq);
 	}
 
 	if (mdwc->ext_xceiv.otg_capability) {
